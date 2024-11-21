d228969c7fc9a89852c55ec8e79e6620eeec8156
MNawfal24 <m.nawfal.mawla@gmail.com>
2024-11-21 10:29:47 +0700

    final commit

--- hitungTotalHarga19.java
+++ hitungTotalHarga19.java
@@ -18,18 +18,42 @@ public class hitungTotalHarga19 {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("\nMasukkan nomor menu yang ingin anda pesan");
+        int totalHargaKeseluruhan = 0; 
+        String keadaan;
+
+        System.out.println("===== MENU KAFE =====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappucino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("=====================");
+        
+        do {System.out.println("\nMasukkan nomor menu yang ingin Anda pesan:");
         int pilihanMenu = sc.nextInt();
-        System.out.println("Masukkan jumlah item yang ingin anda pesan");
+
+        System.out.println("Masukkan jumlah item yang ingin Anda pesan:");
         int banyakItem = sc.nextInt();
 
-         sc.nextLine();
+        sc.nextLine();
 
-        System.out.println(" Apakah ada kode promo ?, jika ada silahkan memasukkannya, jika tidak ada ketik (skip)");
+        System.out.println("Apakah ada kode promo? Jika ada, masukkan; jika tidak, ketik 'SKIP':");
         String kodePromo = sc.nextLine();
 
-        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem, kodePromo);
+        int totalHargaPesanan = hitungTotalHarga(pilihanMenu, banyakItem, kodePromo);
+        totalHargaKeseluruhan += totalHargaPesanan;
 
-        System.out.println("Total harga untuk pesanan anda: Rp"+ totalHarga);
+        System.out.println("Total harga sementara: Rp " + totalHargaKeseluruhan);
+
+       
+        System.out.println("Apakah Anda ingin memesan menu lain? (YA/TIDAK):");
+        keadaan = sc.nextLine();
+    } while (keadaan.equalsIgnoreCase("YA"));
+        
+
+       
+
+        System.out.println("Total harga untuk pesanan anda: Rp"+ totalHargaKeseluruhan);
     }
 }
